eff8750730dbaf8d1092ede286eed92c8ae13cdb
jaderson.brandao <jaderson.brandao@gmail.com>
2025-02-27 23:05:49 -0300

    feat<Greeting> Setup Spring boot application

---
+++ rest-with-spring-boot/src/main/java/br/com/jadersoncardoso/model/Greeting.java
@@ -0,0 +1,4 @@
+package br.com.jadersoncardoso.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot/src/test/java/br/com/jadersoncardoso/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.jadersoncardoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
